d683c92f5e4e262a377c5d8408ca015046c65a9f
Nikolas <thenickilp@web.de>
2021-11-05 08:15:59 +0000

    Added redirect to /friends ...

--- src/main/java/net/fununity/friendsystem/commands/FriendCommand.java
+++ src/main/java/net/fununity/friendsystem/commands/FriendCommand.java
@@ -1,5 +1,6 @@
 package net.fununity.friendsystem.commands;
 
+import java.lang.StringBuilder;
 import net.fununity.friendsystem.language.TranslationKeys;
 import net.fununity.main.api.bungee.command.handler.APICommand;
 import net.fununity.main.api.bungee.player.APIPlayer;
@@ -12,7 +13,14 @@ public class FriendCommand extends APICommand {
     }
 
     @Override
-    public void onCommand(APIPlayer apiPlayer, String[] strings) {
+    public void onCommand(APIPlayer apiPlayer, String[] args) {
+        if (args.length != 0) {
+            StringBuilder builder = new StringBuilder();
+            for(String a : args)
+                builder.append(a).append(" ");
+            Bukkit.dispatchCommand(apiPlayer.getPlayer(), "friends " + builder.toString());
+            return;
+        }
         apiPlayer.sendMessage(TranslationKeys.FRIEND_COMMAND_FRIEND_HELP);
     }
 
